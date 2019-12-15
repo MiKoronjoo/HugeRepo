c63f8c2717a89b39868f375fb19f6469e8da20e4
Hassan <hassan.abbp@gmail.com>
2019-12-15 23:35:48 +0330

    Add part 181 to unix events

--- unix_events.py
+++ unix_events.py
@@ -360,3 +360,5 @@ class _UnixSelectorEventLoop(selector_events.BaseSelectorEventLoop):
 
     async def _make_subprocess_transport(self, protocol, args, shell,
 
+                                         stdin, stdout, stderr, bufsize,
+
