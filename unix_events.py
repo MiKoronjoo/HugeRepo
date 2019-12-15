116eb0686fd3a2dd9c11880d092239cbab4a53f1
Hassan <hassan.abbp@gmail.com>
2019-12-15 23:35:49 +0330

    Add part 273 to unix events

--- unix_events.py
+++ unix_events.py
@@ -544,3 +544,5 @@ class _UnixSelectorEventLoop(selector_events.BaseSelectorEventLoop):
 
                 raise ValueError(
 
+                    'path and sock can not be specified at the same time')
+
